0a3b40ebc69f2b1d10b4cf23defe9449499e3a3e
Daren Yong <daren.yong@sjrb.ca>
2018-03-09 13:31:07 -0700

    add edmonton timezone

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var waterlineConfig = require('./waterline-config');
 var http = require('http');
 var express = require('express');
 var config = require('config');
-var moment = require('moment');
+var moment = require('moment-timezone');
 var routes = require('./routes');
 
 const port = process.env.port || '8080';
@@ -23,7 +23,7 @@ Waterline.start(waterlineConfig, function (err, orm) {
   app.use(function (req, res, next) {
     var url = decodeURI(req.url);
     var query = JSON.stringify(req.query);
-    console.log(moment().format('YYYY-MM-DD HH:mm:ss'),'method', req.method, 'url', url, 'query', query);
+    console.log(moment().tz('America/Edmonton').format('YYYY-MM-DD HH:mm:ss'),'method', req.method, 'url', url, 'query', query);
 
     var err = new Error('Not Found');
     err.title = 'Not Found';
