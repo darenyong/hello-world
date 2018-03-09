c269d97b9b674a881ef1950ca623745c61320ceb
Daren Yong <daren.yong@sjrb.ca>
2018-03-09 14:28:57 -0700

    add log

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment-timezone');
 
 // home page
 router.get('/', function (req, res, next) {
-  res.json({title: 'hello-world'})
-})
+  console.log('GET hello', moment().tz('America/Edmonton').format('YYYY-MM-DD HH:mm:ss'));
+  res.json({title: 'hello-world'});
+});
 
 module.exports = router;
