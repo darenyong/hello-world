abe669706abcf6537fc57b9d7f4a6b2415abf4a0
Daren Yong <daren.j.yong@gmail.com>
2018-03-10 10:51:36 -0700

    add test /jenkins

---
+++ jenk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker stop jenkins
+docker rm jenkins
+
+docker run -d --name jenkins \
+  -l "traefik.frontend.whitelistSourceRange=184.64.12.12" \
+  -l "traefik.backend=jenkins" \
+  -l "traefik.frontend.rule=Host:darenyong.com,www.darenyong.com;PathPrefixStrip:/jenkins" \
+  -l "traefik.enable=true" \
+  -l "traefik.port=8080" \
+  --network proxy \
+  -e "NODE_ENV=development" \
+  my-hello:1.0.0
