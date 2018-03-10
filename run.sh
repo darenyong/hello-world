c74766e2ca16ae31435fb0b9abda68647d228362
Daren Yong <dyong@localhost.localdomain>
2018-03-10 01:21:11 -0700

    add traefik labels

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+docker stop my-hello
+docker rm my-hello
+
 docker run -d --name my-hello \
-  --network web \
+  -l "traefik.backend=my-hello" \
+  -l "traefik.frontend.rule=Host:darenyong.com" \
+  -l "traefik.enable=true" \
+  -l "traefik.port=8080" \
+  --network proxy \
   -p 8080:8080 \
   -e "NODE_ENV=development" \
   my-hello:1.0.0
+
+  #-l "traefik.frontend.rule=PathStrip:/hello" \
