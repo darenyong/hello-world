ca3e86499cdf06a413fab6570b86e3c43c1d0bdf
Daren Yong <daren.yong@sjrb.ca>
2018-05-30 18:17:11 -0600

    add decode token test

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-var Waterline = require('waterline');
-var waterlineConfig = require('./waterline-config');
+// var Waterline = require('waterline');
+// var waterlineConfig = require('./waterline-config');
 var http = require('http');
 var express = require('express');
 var config = require('config');
 var moment = require('moment-timezone');
+var cookieParser = require('cookie-parser');
 var routes = require('./routes');
 
 const port = process.env.port || '8080';
 
 console.log('starting waterline', moment().tz('America/Edmonton').format('YYYY-MM-DD HH:mm:ss'));
-Waterline.start(waterlineConfig, function (err, orm) {
-  if (err) {
-    console.error('could not start waterline\n', err);
-    return process.exit(1);
-  }
+
+// Waterline.start(waterlineConfig, function (err, orm) {
+//   if (err) {
+//     console.error('could not start waterline\n', err);
+//     return process.exit(1);
+//   }
 
   var app = express();
-
+  app.use(cookieParser());
   app.use('/', routes);
 
   // catch 404 and forward to error handler
@@ -47,34 +49,34 @@ Waterline.start(waterlineConfig, function (err, orm) {
   server.listen(port, function (err) {
     if (err) {
       console.error('failed to lift express');
-      console.error('attempting to shutdown waterline');
-      Waterline.stop(orm, function (err) {
-        console.error('unexpected failure attempting to shutdown waterline. Details:', err);
-        return process.exit(1);
-      });
-      console.info('waterline shutdown successfully');
+      // console.error('attempting to shutdown waterline');
+      // Waterline.stop(orm, function (err) {
+      //   console.error('unexpected failure attempting to shutdown waterline. Details:', err);
+      //   return process.exit(1);
+      // });
+      // console.info('waterline shutdown successfully');
       return process.exit(1);
     }
 
     // success starting express
-    console.log('db config\n', config.dbConfig);
+    // console.log('db config\n', config.dbConfig);
     console.log('express running');
 
-    var message = Waterline.getModel('message', orm);
+    // var message = Waterline.getModel('message', orm);
 
     // TODO: test create a message
-    message
-      .create({text: 'hello-world'})
-      .meta({fetch: true})
-      .exec(function (err, msg) {
-        if (err) return console.error('error creating message');
-        console.log('created', msg);
-      });
+    // message
+    //   .create({text: 'hello-world'})
+    //   .meta({fetch: true})
+    //   .exec(function (err, msg) {
+    //     if (err) return console.error('error creating message');
+    //     console.log('created', msg);
+    //   });
 
-    message.find().exec(function (err, msgs) {
-      console.log('messages', msgs);
-    });
+    // message.find().exec(function (err, msgs) {
+    //   console.log('messages', msgs);
+    // });
 
-  });
+  // });
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment-timezone');
+var jwt = require('jsonwebtoken');
+var fs = require('fs');
+var path = require('path');
+var publicKey =  fs.readFileSync(path.join(__dirname, '..', 'darenyong.pem'), 'utf-8');
+
+
+// var secret = jwks.expressJwtSecret({
+//   cache: true,
+//   rateLimit: true,
+//   jwksRequestsPerMinute: 5,
+//   jwksUri: "https://darenyong.auth0.com/.well-known/jwks.json"
+// });
 
 // home page
 router.get('/', function (req, res, next) {
   console.log('GET hello', moment().tz('America/Edmonton').format('YYYY-MM-DD HH:mm:ss'));
-  res.json({title: 'hello-world'});
+
+  console.log('publicKey', publicKey);
+  console.log('cookies', req.cookies);
+  const cookie = req.cookies['daren-auth-token'];
+  console.log('daren-auth-token', cookie);
+
+  jwt.verify(cookie, publicKey, { algorithms: ['RS256'] }, function(err, decoded) {
+    if (err) {
+      console.log('verify token failed');
+      res.status(400);
+      res.json({msg: 'failed to verify token'});
+      return;
+    }
+    console.log('decoded', decoded);
+    res.json({title: 'hello-world'});
+  });
 });
 
 module.exports = router;
