6eb94aa8255a786fde40a5be081cc11a92081b8f
Daren Yong <daren.yong@sjrb.ca>
2018-06-02 00:18:17 -0600

    clean up

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 # clean out old containers and images before build
 docker stop my-hello
 docker rm my-hello
-docker rmi my-hello:1.0.0
+
+# for now leave garbage behind for faster build
+# docker rmi my-hello:1.0.0
 
 VERSION="1.0.0"
 tag="my-hello:${VERSION}"
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var moment = require('moment-timezone');
+// var moment = require('moment-timezone');
 var jwt = require('jsonwebtoken');
 var fs = require('fs');
 var path = require('path');
-var publicKey =  fs.readFileSync(path.join(__dirname, '..', 'darenyong.pem'), 'utf-8');
 
-
-// var secret = jwks.expressJwtSecret({
-//   cache: true,
-//   rateLimit: true,
-//   jwksRequestsPerMinute: 5,
-//   jwksUri: "https://darenyong.auth0.com/.well-known/jwks.json"
-// });
+var publicKey = fs.readFileSync(path.join(__dirname, '..', 'darenyong.pem'), 'utf-8');
 
 // home page
 router.get('/', function (req, res, next) {
-  console.log('GET hello', moment().tz('America/Edmonton').format('YYYY-MM-DD HH:mm:ss'));
-
+  // console.log('GET hello', moment().tz('America/Edmonton').format('YYYY-MM-DD HH:mm:ss'));
   console.log('publicKey', publicKey);
   console.log('cookies', req.cookies);
   const cookie = req.cookies['daren-auth-token'];
   console.log('daren-auth-token', cookie);
 
+  if (!cookie) {
+    res.status(401);
+    res.send('hello-world no cookie provided');
+    return;
+  }
+
   jwt.verify(cookie, publicKey, { algorithms: ['RS256'] }, function(err, decoded) {
     if (err) {
       console.log('verify token failed');
-      res.status(400);
-      res.json({msg: 'failed to verify token'});
+      res.status(401);
+      res.send('token not valid or expired');
       return;
     }
-    console.log('decoded', decoded);
+    // TODO: token - confirm issuer, audience, and scope
+    console.log('decoded token', decoded);
     res.json({title: 'hello-world'});
   });
 });
